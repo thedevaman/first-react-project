c01b8bca636ada772b5a78d788ef727e912bfef1
thedevaman <devaman2508@gmail.com>
2022-02-22 09:28:26 +0530

    undestanding jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Aman"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <> 
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </nav>
+    <div className='container'>
+      <h1>welcome to react With {name}</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio harum quidem dolores nisi atque tenetur! Molestiae, vitae reiciendis sed doloremque blanditiis tempora deserunt vel laudantium odit commodi repellat, repellendus voluptate?</p>
     </div>
-  );
+    </>
+   
+  ); 
 }
 
 export default App;
