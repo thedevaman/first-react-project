05f6ba469223da94103e51e0615800110ffa09c4
thedevaman <devaman2508@gmail.com>
2022-02-25 09:56:33 +0530

    adding components props proptypes defaultprops

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
-let name = "Aman"
+
 function App() {
   return (
     <> 
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className='container'>
-      <h1>welcome to react With {name}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio harum quidem dolores nisi atque tenetur! Molestiae, vitae reiciendis sed doloremque blanditiis tempora deserunt vel laudantium odit commodi repellat, repellendus voluptate?</p>
-    </div>
+  
+  <Navbar title ="TextUtils"/>
+
+  {/* <Navbar/> */}
+
     </>
    
   ); 
